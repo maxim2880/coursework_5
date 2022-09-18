b9e71389c153cba6d6b7dfb0a6eb358e2d0c58d1
admin <misuramaxim@gmail.com>
2022-09-18 12:36:49 +0500

    Done work

--- help files/app.py
+++ help files/app.py
@@ -1,3 +1,5 @@
+from flask import render_template, Flask
+
 app = Flask(__name__)
 
 heroes = {
--- skills.py
+++ skills.py
@@ -5,13 +5,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from unit import BaseUnit
 
-class Skill(ABC):
-    """
-    Базовый класс умения
-    """
-    user = None
-    target = None
 
+class SkillABC(ABC):
     @property
     @abstractmethod
     def name(self):
@@ -27,9 +22,21 @@ class Skill(ABC):
     def damage(self):
         pass
 
-    @abstractmethod
+
+class Skill(SkillABC):
+    """
+    Базовый класс умения
+    """
+    user = None
+    target = None
+    name = None
+    stamina = None
+    damage = None
+
     def skill_effect(self) -> str:
-        pass
+        self.user.stamina -= self.stamina
+        self.target.get_damage(self.damage)
+        return f"{self.user.name} использует {self.name} и наносит {self.damage} урон сопернику"
 
     def _is_stamina_enough(self):
         return self.user.stamina > self.stamina
@@ -47,22 +54,12 @@ class Skill(ABC):
 
 
 class FuryPunch(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
+    name = "Свирепый пинок"
+    stamina = 6
+    damage = 12
 
-    def skill_effect(self):
-        # TODO логика использования скилла -> return str
-        # TODO в классе нам доступны экземпляры user и target - можно использовать любые их методы
-        # TODO именно здесь происходит уменшение стамины у игрока применяющего умение и
-        # TODO уменьшение здоровья цели.
-        # TODO результат применения возвращаем строкой
-        pass
 
 class HardShot(Skill):
-    name = ...
-    stamina = ...
-    damage = ...
-
-    def skill_effect(self):
-        pass
+    name = "Мощный укол"
+    stamina = 5
+    damage = 15
